a19abe20cbc48c12bbf4cdfd18e56c65e6a547a3
thesuss <lullisuss@gmail.com>
2016-09-11 12:07:35 +0200

    commented out email validation as this is not used in production

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,15 @@
-#found here:http://guides.rubyonrails.org/active_record_validations.html
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
+# #found here:http://guides.rubyonrails.org/active_record_validations.html
+# class EmailValidator < ActiveModel::EachValidator
+#   def validate_each(record, attribute, value)
+#     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+#       record.errors[attribute] << (options[:message] || "is not an email")
+#     end
+#   end
+# end
 
 class Comment < ApplicationRecord
   belongs_to :article
   validates :commenter, presence: true
   validates :body, presence: true
-  validates :email, presence: true, email: true
+  validates :email, presence: true#, email: true
 end
